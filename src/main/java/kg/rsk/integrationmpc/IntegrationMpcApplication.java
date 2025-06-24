01bab2c0610730cd575e4629456fdd67dd61ff75
Maksatbek Bolushov <m.bolushov@int.rsktech.kg>
2025-06-24 10:29:49 +0600

    first commit

---
+++ src/main/java/kg/rsk/integrationmpc/IntegrationMpcApplication.java
@@ -0,0 +1,13 @@
+package kg.rsk.integrationmpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationMpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationMpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/rsk/integrationmpc/IntegrationMpcApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.rsk.integrationmpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationMpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
